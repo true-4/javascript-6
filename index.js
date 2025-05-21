9790db7ca4ef3d7e5a55a50960fba74d67d63402
kuga <esixterb58@gmail.com>
2025-05-21 18:56:49 +0300

    feat: task-1 use strict

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-// no 'use strict'
+'use strict'
+
 console.log(this) // выводит window ссылаясь на окружение документа
 
 function test() {
-  console.log(this) // выводит window такой же как и просто вызов в примере выше
+  console.log(this) // выводит undefined так как функция ничего не возвращает в своём Lexical Environment
 }
 test()
 
 const test1 = () => this
-console.log(test1()) // выводит window ссылаясь на окружение документа
+console.log(test1()) // выводит window ссылаясь на окружение документа так как не имеет собственного this и обращается к this родителя
