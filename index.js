856243df9056e3e59b8cc066cd7e4b1bfa064de0
kuga <esixterb58@gmail.com>
2025-05-22 19:00:36 +0300

    feat: task calc

--- index.js
+++ index.js
@@ -23,12 +23,12 @@ console.log(result) // выводит результат 6
 
 // реализация 2 вариант
 const compose = (...rest) => { // объявляем стрелочную функцию с любым количеством параметров которые записываются в rest
-  return (num) => {
-    let res = num
-    for (let i = rest.length - 1; i >= 0; i--) {
-      res = rest[i](res) // 
+  return (num) => { // возвращаем функцию с параметром 
+    let res = num // объявляем переменную res счётчик которая будет хранить результат цикла в значении параметра num
+    for (let i = rest.length - 1; i >= 0; i--) { // объявляем цикл который проходится по значениям rest в обратном порядке
+      res = rest[i](res) // присаеваем в res результат введённы значений из rest и вызываем значение которое мы передаём
     }
-    return res
+    return res // возвращаем результат цикла которая присваевается в num
   }
 }
 
@@ -37,4 +37,31 @@ const result1 = compose(x, y, z)(3) // записываем результат �
 console.log(result1) // выводит результат 6
 
 // 3
-// 3.1
+function Calc() {
+  this.result = 0 // устанавливаю значение по умолчанию
+  this.add = num => this.result += num // сложение которое записывается в result
+  this.subtract = num => this.result -= num // вычитание
+  this.multiply = num => this.result *= num // умножение
+  this.divide = num => this.result /= num // деление
+  this.reset = () => this.result = 0 // сброс 
+  this.getValue = () => this.result // возвращает актуальное значение в result
+}
+
+const useCalc = new Calc() // собираем конструктором функцию Calc
+
+console.log(useCalc.result) // проверяю значение по умолчанию
+
+useCalc.add(100) // передаём значение на сложение
+console.log(useCalc.getValue()) // проверяем актуальное значение в result
+
+useCalc.subtract(8) // передаём значение на вычитание 
+console.log(useCalc.getValue())
+
+useCalc.multiply(2) // передаём значение на умножение
+console.log(useCalc.getValue())
+
+useCalc.divide(4) // передаём значение на деление
+console.log(useCalc.getValue())
+
+useCalc.reset() // обнуляет результат присваивая 0
+console.log(useCalc.getValue())
